7e27d7ed77dba211387152607cb65a371c75fefa
Craun718 <3653544699@qq.com>
2026-09-19 17:43:55 +0800

    fix(android): use TwelveMonkeys BMP ICO reader

--- src-tauri/gen/android/buildSrc/build.gradle.kts
+++ src-tauri/gen/android/buildSrc/build.gradle.kts
@@ -23,7 +23,8 @@ dependencies {
     implementation("com.android.tools.build:gradle:9.3.2")
     implementation("org.tomlj:tomlj:1.3.0")
     implementation("org.checkerframework:checker-qual:3.55.1")
-    implementation("org.twelvemonkeys.imageio:image-ico:3.12.0")
-    implementation("org.twelvemonkeys.imageio:imageio-webp:3.12.0")
+    // TwelveMonkeys' current ICO reader is provided by the BMP plugin.
+    implementation("com.twelvemonkeys.imageio:imageio-bmp:3.12.0")
+    implementation("com.twelvemonkeys.imageio:imageio-webp:3.12.0")
     testImplementation("junit:junit:4.13.2")
 }

2f36045e80c369c77de6619044aade7037be64b5
Craun718 <3653544699@qq.com>
2026-09-20 09:07:36 +0800

    fix(android): remove duplicate import in PiLauncherIcon

--- src-tauri/gen/android/buildSrc/src/main/java/top/natsuu/mta/kotlin/PiLauncherIcon.kt
+++ src-tauri/gen/android/buildSrc/src/main/java/top/natsuu/mta/kotlin/PiLauncherIcon.kt
@@ -2,10 +2,9 @@ package top.natsuu.mta.kotlin
 
 import groovy.json.JsonSlurper
 import java.awt.Graphics2D
-import java.awt.geom.Ellipse2D
 import java.awt.RenderingHints
-import java.awt.image.BufferedImage
 import java.awt.geom.Ellipse2D
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
